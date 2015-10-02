4c991daa679c5f232f3761cde1a5031541e6f732
Matthias Metzger <noobymatze@yahoo.de>
2015-10-02 16:54:17 +0200

    added implementation for freeVars and substVars

---
+++ Data/Expr/Proposition/Substitute.hs
@@ -0,0 +1,25 @@
+module Data.Expr.Proposition.Substitute where
+
+import Data.List      (union)
+
+import Data.Expr.Proposition.Types
+
+-- ----------------------------------------
+-- variable substitution
+
+type VarEnv = [(Ident, Expr)]
+
+substVars :: VarEnv -> Expr -> Expr
+substVars _ x@(Lit _)         = x
+substVars env (Var a)         = snd . head . filter ((==a) . fst) $ env
+substVars env (Unary op a)    = Unary op (substVars env a)
+substVars env (Binary op a b) = Binary op (substVars env a) (substVars env b)
+
+
+freeVars :: Expr -> [Ident]
+freeVars (Lit _)        = []
+freeVars (Var a)        = [a]
+freeVars (Unary _ a)    = freeVars a
+freeVars (Binary _ a b) = union (freeVars a) (freeVars b)
+
+-- ----------------------------------------
