590c3170423d3819b54b6d7cd88e3e06ef8fcfba
Matthias Metzger <noobymatze@yahoo.de>
2015-10-04 16:39:07 +0200

    added implementation for word count

---
+++ WordCount0.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE UnboxedTuples #-}
+
+module WordCount0 where
+
+-- the working horse
+-- with predefined type Sum for (+,0) monoid 
+import           Data.Monoid
+
+-- the more effizent Text representation
+-- than the native String type
+import qualified Data.Text          as T
+import qualified Data.Text.IO       as T
+
+import           System.Environment (getArgs)
+
+-- ----------------------------------------
+--
+-- the whole main program
+
+main :: IO ()
+main
+  = do (inp :_) <- getArgs
+       text     <- T.readFile inp
+       writeResult inp (processText text)
+       return ()
+
+-- --------------------
+
+type Counters
+  = (Sum Int,              -- line count
+     (Sum Int,             -- word count
+      (Sum Int,            -- char count
+       ())))
+
+zeros :: Counters
+zeros = (Sum 0, (Sum 0, (Sum 0, ()))) 
+
+-- --------------------
+--
+-- the whole computation
+
+processText :: T.Text -> Counters
+processText t
+  = foldr (mappend . toCounters) zeros . T.lines $ t
+
+-- process a single line
+toCounters :: T.Text -> Counters
+toCounters text = (Sum 1,
+                   (Sum . length . T.words $ text,
+                    (Sum . T.length $ text,
+                     ())))
+
+-- --------------------
+--
+-- the boring formatting of the results
+
+writeResult :: String -> Counters -> IO ()
+writeResult f (Sum lc, (Sum wc, (Sum cc, ())))
+  = putStrLn $ unlines $
+    [ "Statistics for " ++ show f
+    , "lines        : " ++ fillI8 lc
+    , "words        : " ++ fillI8 wc
+    , "chars        : " ++ fillI8 (cc + lc)
+    ]
+  where
+    fillI8 = fillLeft 8 . show
+
+    fillLeft n v
+      = replicate ((n - m) `max` 0) ' ' ++ v
+        where
+          m = length v
+
+-- ----------------------------------------
